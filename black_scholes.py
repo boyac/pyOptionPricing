52d1cb37c4ac8c17281476d72f5e859f53a88280
Boya Chiou <boya.chiou@gmail.com>
2019-09-11 13:43:27 +0800

    Update black_scholes.py

--- black_scholes.py
+++ black_scholes.py
@@ -35,4 +35,4 @@ def BlackScholes(CallPutFlag,S,K,t,r,s):
 
 if __name__ == "__main__":
     # Number taken from: http://wiki.mbalib.com/wiki/Black-Scholes期权定价模型
-    print BlackScholes('c', 164.0, 165.0, 0.0959, 0.0521, 0.0841) # 5.788529972549341
+    print BlackScholes('c', S0=164.0, K=165.0, t=0.0959, r=0.0521, v=0.29) # 5.788529972549341
