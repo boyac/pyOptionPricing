e7c47f40965277dab557e366122ba4cf293fbe7c
Boya Chiou <boya.chiou@gmail.com>
2019-09-12 08:55:42 +0800

    monte_carlo() rewrite

--- monte_carlo.py
+++ monte_carlo.py
@@ -13,12 +13,12 @@ def generate_asset_price(S,v,r,T):
 def call_payoff(S_T,K):
 	return max(0.0, S_T-K) # (S_T - K) = Intrinsic value for call
 
-def monte_carlo(S,K,T,r,v,simulations):
+def monte_carlo(simulations):
 	payoffs = []
 	discount_factor = exp(-r * T)
 
 	for i in xrange(simulations):
-		S_T = generate_asset_price(S,T,r,v)
+		S_T = generate_asset_price(S,v,r,T)
 		payoffs.append(call_payoff(S_T,K))
 
 	price = discount_factor * (sum(payoffs) / float(simulations))
@@ -26,5 +26,10 @@ def monte_carlo(S,K,T,r,v,simulations):
 
 
 if __name__ == '__main__':
-	print monte_carlo(100,80,1,0.5,0.15,100000)
+	S = 761.68
+	K = 680
+	t = 1.1479
+	r = 0.04
+	v = 0.3232
+	print monte_carlo(100000)
 	# read more about Monte_Carlo on investment: http://wiki.mbalib.com/wiki/%E8%92%99%E7%89%B9%E5%8D%A1%E7%BD%97%E6%96%B9%E6%B3%95
